2bc3085c7b56e3f3adab343ce6adacd04d60f079
nick avlo <avlo@yahoo.com>
2024-04-18 15:58:05 -0700

    formatting

--- src/main/java/com/github/rawsanj/handler/ChatWebSocketHandler.java
+++ src/main/java/com/github/rawsanj/handler/ChatWebSocketHandler.java
@@ -15,53 +15,53 @@ import reactor.core.publisher.Sinks;
 @Slf4j
 public class ChatWebSocketHandler implements WebSocketHandler {
 
-	private final Sinks.Many<ChatMessage> chatMessageSink;
-	private final Flux<ChatMessage> chatMessageFluxSink;
-	private final RedisChatMessagePublisher redisChatMessagePublisher;
-	private final RedisAtomicLong activeUserCounter;
-	private final ObjectStringConverter objectStringConverter;
+  private final Sinks.Many<ChatMessage> chatMessageSink;
+  private final Flux<ChatMessage> chatMessageFluxSink;
+  private final RedisChatMessagePublisher redisChatMessagePublisher;
+  private final RedisAtomicLong activeUserCounter;
+  private final ObjectStringConverter objectStringConverter;
 
-	public ChatWebSocketHandler(Sinks.Many<ChatMessage> chatMessageSink, RedisChatMessagePublisher redisChatMessagePublisher,
-								RedisAtomicLong activeUserCounter, ObjectStringConverter objectStringConverter) {
-		this.chatMessageSink = chatMessageSink;
-		this.chatMessageFluxSink = chatMessageSink.asFlux();
-		this.redisChatMessagePublisher = redisChatMessagePublisher;
-		this.activeUserCounter = activeUserCounter;
-		this.objectStringConverter = objectStringConverter;
-	}
+  public ChatWebSocketHandler(Sinks.Many<ChatMessage> chatMessageSink, RedisChatMessagePublisher redisChatMessagePublisher,
+      RedisAtomicLong activeUserCounter, ObjectStringConverter objectStringConverter) {
+    this.chatMessageSink = chatMessageSink;
+    this.chatMessageFluxSink = chatMessageSink.asFlux();
+    this.redisChatMessagePublisher = redisChatMessagePublisher;
+    this.activeUserCounter = activeUserCounter;
+    this.objectStringConverter = objectStringConverter;
+  }
 
-	@Override
-	public Mono<Void> handle(WebSocketSession webSocketSession) {
-		Flux<WebSocketMessage> sendMessageFlux = chatMessageFluxSink.flatMap(objectStringConverter::objectToString)
-			.map(webSocketSession::textMessage)
-			.doOnError(throwable -> log.error("Error Occurred while sending message to WebSocket.", throwable));
-		Mono<Void> outputMessage = webSocketSession.send(sendMessageFlux);
+  @Override
+  public Mono<Void> handle(WebSocketSession webSocketSession) {
+    Flux<WebSocketMessage> sendMessageFlux = chatMessageFluxSink.flatMap(objectStringConverter::objectToString)
+        .map(webSocketSession::textMessage)
+        .doOnError(throwable -> log.error("Error Occurred while sending message to WebSocket.", throwable));
+    Mono<Void> outputMessage = webSocketSession.send(sendMessageFlux);
 
-		Mono<Void> inputMessage = webSocketSession.receive()
-			.flatMap(webSocketMessage -> redisChatMessagePublisher.publishChatMessage(webSocketMessage.getPayloadAsText()))
-			.doOnSubscribe(subscription -> {
-				long activeUserCount = activeUserCounter.incrementAndGet();
-				log.info("User '{}' Connected. Total Active Users: {}", webSocketSession.getId(), activeUserCount);
-				chatMessageSink.tryEmitNext(new ChatMessage(0, "CONNECTED", "CONNECTED", activeUserCount));
-			})
-			.doOnError(throwable -> log.error("Error Occurred while sending message to Redis.", throwable))
-			.doFinally(signalType -> {
-				long activeUserCount = activeUserCounter.decrementAndGet();
-				log.info("User '{}' Disconnected. Total Active Users: {}", webSocketSession.getId(), activeUserCount);
-				chatMessageSink.tryEmitNext(new ChatMessage(0, "DISCONNECTED", "DISCONNECTED", activeUserCount));
-			})
-			.then();
+    Mono<Void> inputMessage = webSocketSession.receive()
+        .flatMap(webSocketMessage -> redisChatMessagePublisher.publishChatMessage(webSocketMessage.getPayloadAsText()))
+        .doOnSubscribe(subscription -> {
+          long activeUserCount = activeUserCounter.incrementAndGet();
+          log.info("User '{}' Connected. Total Active Users: {}", webSocketSession.getId(), activeUserCount);
+          chatMessageSink.tryEmitNext(new ChatMessage(0, "CONNECTED", "CONNECTED", activeUserCount));
+        })
+        .doOnError(throwable -> log.error("Error Occurred while sending message to Redis.", throwable))
+        .doFinally(signalType -> {
+          long activeUserCount = activeUserCounter.decrementAndGet();
+          log.info("User '{}' Disconnected. Total Active Users: {}", webSocketSession.getId(), activeUserCount);
+          chatMessageSink.tryEmitNext(new ChatMessage(0, "DISCONNECTED", "DISCONNECTED", activeUserCount));
+        })
+        .then();
 
-		return Mono.zip(inputMessage, outputMessage).then();
-	}
+    return Mono.zip(inputMessage, outputMessage).then();
+  }
 
-	public Mono<Sinks.EmitResult> sendMessage(ChatMessage chatMessage) {
-		return Mono.fromSupplier(() -> chatMessageSink.tryEmitNext(chatMessage))
-			.doOnSuccess(emitResult -> {
-				if (emitResult.isFailure()) {
-					log.error("Failed to send message with id: {}", chatMessage.getId());
-				}
-			});
-	}
+  public Mono<Sinks.EmitResult> sendMessage(ChatMessage chatMessage) {
+    return Mono.fromSupplier(() -> chatMessageSink.tryEmitNext(chatMessage))
+        .doOnSuccess(emitResult -> {
+          if (emitResult.isFailure()) {
+            log.error("Failed to send message with id: {}", chatMessage.getId());
+          }
+        });
+  }
 
 }
--- src/main/java/com/github/rawsanj/handler/WebHttpHandler.java
+++ src/main/java/com/github/rawsanj/handler/WebHttpHandler.java
@@ -19,17 +19,20 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @Configuration(proxyBeanMethods = false)
 public class WebHttpHandler {
 
-	@Bean
-	public RouterFunction<ServerResponse> htmlRouter(@Value("classpath:/static/index.html") Resource html,
-													 RedisChatMessagePublisher redisChatMessagePublisher, Environment environment) {
-		return route(GET("/"), request -> ServerResponse.ok().contentType(MediaType.TEXT_HTML).bodyValue(html))
-			.andRoute(POST("/message"), request -> request.bodyToMono(Message.class)
-				.flatMap(message -> redisChatMessagePublisher.publishChatMessage(message.getMessage()))
-				.flatMap(aLong -> ServerResponse.ok().bodyValue(new Message("Message Sent Successfully!.")))
-			)
-			.andRoute(
-				GET("/platform"), request -> ServerResponse.ok().bodyValue(new Platform(environment.getProperty("RUNTIME_PLATFORM", "JVM")))
-			);
-	}
+  @Bean
+  public RouterFunction<ServerResponse> htmlRouter(
+      @Value("classpath:/static/index.html") Resource html,
+      RedisChatMessagePublisher redisChatMessagePublisher,
+      Environment environment) {
+
+    return route(GET("/"), request -> ServerResponse.ok().contentType(MediaType.TEXT_HTML).bodyValue(html))
+        .andRoute(POST("/message"), request -> request.bodyToMono(Message.class)
+            .flatMap(message -> redisChatMessagePublisher.publishChatMessage(message.getMessage()))
+            .flatMap(aLong -> ServerResponse.ok().bodyValue(new Message("Message Sent Successfully!.")))
+        )
+        .andRoute(
+            GET("/platform"), request -> ServerResponse.ok().bodyValue(new Platform(environment.getProperty("RUNTIME_PLATFORM", "JVM")))
+        );
+  }
 
 }
